b616c7c90488d41eb11403d5b258e7b5d4132bca
Zayd Dawood <zdawood@glos.ac.uk>
2023-11-14 13:31:20 +0000

    Added Health Manager and Timer

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Killzone : MonoBehaviour
+public class Restart : MonoBehaviour
 {
     private void OnTriggerEnter2D ( Collider2D collision )
     {
